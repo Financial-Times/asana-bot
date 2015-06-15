dd81432db6a5db4d202430011498bb0d13028267
Lucas Gluchowski <lucas.gluchowski@ft.com>
2015-06-15 17:42:39 +0100

    Fixed failing test after for controller.buildReportDate(preferredReportType)

--- src/test/groovy/com/ft/report/ReportsControllerSpec.groovy
+++ src/test/groovy/com/ft/report/ReportsControllerSpec.groovy
@@ -61,8 +61,8 @@ class ReportsControllerSpec extends Specification {
 
         where:
             preferredReportType          | expectedReportDate
-            ReportType.TODAY           | '01/06/2015'
-            ReportType.TOMORROW           | '01/06/2015'
+            ReportType.TODAY             | '01/06/2015'
+            ReportType.TOMORROW          | '02/06/2015'
             ReportType.SUNDAY_FOR_MONDAY | '07/06/2015 - 08/06/2015'
     }
 

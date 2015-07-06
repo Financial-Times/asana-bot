f09043a94203bd5b5082d5f7131a3f2c4d7cf7ca
FTGremlin <mo.shawwa@ft.com>
2015-07-06 16:42:23 +0100

    - created page objects   - created BypassAuthenticationSpec.groovy test   - added downloading chromedriver before tests   - added chromedriver location to .gitignore   - added login env variables   - added mvn test to circleci   - created AsanaLogin.groovy in preparation for next phase

---
+++ src/test/groovy/com/ft/geb/pages/ConferenceReport.groovy
@@ -0,0 +1,24 @@
+package com.ft.geb.pages
+
+import geb.Page
+
+class ConferenceReport extends Page{
+
+    static url = "http://localhost:8080/"
+    static at = { title == "Request for Permission" }
+    static content = {
+        acceptButton { $('#submit_approve_access') }
+        acceptButtonDisabled { $('#submit_approve_access').getAttribute('disabled') }
+
+        topFrame { $('body h1') }
+        selectText { $('body h3') }
+        reportType { $("#reportTypeSelect") }
+        deskType { $("#teamSelect") }
+        submitButton { $("#submitButton") }
+
+        standardText { $("body header span") }
+        displayedDeskText { $("body header span:nth-child(2)") }
+
+        fullReportInformation { $('body div[class="panel-body printable"] div') }
+        }
+}

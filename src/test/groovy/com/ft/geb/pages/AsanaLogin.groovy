8cd90be14a69352663d1fbd462d88ac13a3fe29f
FTGremlin <mo.shawwa@ft.com>
2015-07-06 16:42:23 +0100

    - created page objects   - created BypassAuthenticationSpec.groovy test   - added downloading chromedriver before tests   - added chromedriver location to .gitignore   - added login env variables   - added mvn test to circleci   - created AsanaLogin.groovy in preparation for next phase

--- src/test/groovy/com/ft/geb/pages/AsanaLogin.groovy
+++ src/test/groovy/com/ft/geb/pages/AsanaLogin.groovy
@@ -1,4 +1,12 @@
 package com.ft.geb.pages
 
-class AsanaLogin {
+import geb.Page
+
+class AsanaLogin extends Page {
+
+    static url = "https://app.asana.com/0/41466326532110/41466326532110"
+    static at = "● Tasks Assigned to Me - Asana"
+    static content = {
+
+    }
 }

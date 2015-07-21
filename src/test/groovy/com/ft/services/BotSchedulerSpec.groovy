76e80bad84732db24175d57fda951fdd628e039a
Lucas Gluchowski <lucas.gluchowski@ft.com>
2015-07-21 17:31:39 +0100

    DN-184: Added scheduler for Asana changes notification.

--- src/test/groovy/com/ft/services/BotSchedulerSpec.groovy
+++ src/test/groovy/com/ft/services/BotSchedulerSpec.groovy
@@ -1,6 +1,8 @@
 package com.ft.services
 
 import com.ft.backup.AsanaBackupService
+import com.ft.monitoring.AsanaChangesService
+import com.ft.monitoring.ProjectChange
 import org.junit.Rule
 import org.springframework.boot.test.OutputCapture
 import spock.lang.Specification
@@ -9,6 +11,8 @@ class BotSchedulerSpec extends Specification {
     private BotScheduler botScheduler
     private AsanaService mockAsanaService = Mock(AsanaService)
     private AsanaBackupService mockAsanaBackupService = Mock(AsanaBackupService)
+    private AsanaChangesService mockAsanaChangesService = Mock(AsanaChangesService)
+    private SlackService mockSlackService = Mock(SlackService)
 
     @Rule
     OutputCapture capture = new OutputCapture()
@@ -17,6 +21,8 @@ class BotSchedulerSpec extends Specification {
         botScheduler = new BotScheduler()
         botScheduler.asanaService = mockAsanaService
         botScheduler.asanaBackupService = mockAsanaBackupService
+        botScheduler.asanaChangesService = mockAsanaChangesService
+        botScheduler.slackService = mockSlackService
 
         capture.flush()
     }
@@ -105,5 +111,19 @@ class BotSchedulerSpec extends Specification {
             0 * _
         and:
             capture.toString().contains("java.io.IOException: test message")
+    }
+
+    void "checkForChanges passes changes to slack notifier"() {
+        given:
+            ProjectChange change1 = new ProjectChange(null)
+            ProjectChange change2 = new ProjectChange(null)
+            List<ProjectChange> changes = [change1, change2]
+        when:
+            botScheduler.checkForChanges()
+        then:
+            1 * mockAsanaChangesService.getChanges() >> changes
+            1 * mockSlackService.notifyProjectChange(changes)
+            0 * _
+
     }
 }

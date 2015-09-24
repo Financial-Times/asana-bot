17c0c1eaee468b069c72918721f27be95ef4f8fc
Lucas Gluchowski <lucas.gluchowski@ft.com>
2015-09-24 11:34:06 +0100

    Ignoring LoginFunctCase as password for test account has been changed and is unknown ay the moment.

--- src/test/groovy/com/ft/geb/FunctionalSuiteSpec.groovy
+++ src/test/groovy/com/ft/geb/FunctionalSuiteSpec.groovy
@@ -3,11 +3,11 @@ package com.ft.geb
 import com.ft.geb.tests.LoginFunctCase
 import org.junit.runner.RunWith
 import org.junit.runners.Suite
+import spock.lang.Ignore
 
 @RunWith(Suite.class)
 @Suite.SuiteClasses([
-        LoginFunctCase.class,
+        //LoginFunctCase.class, commenting this out since password for test account has been changed
 ])
-
 class FunctionalSuiteSpec {
 }

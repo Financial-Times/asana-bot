8b18060ebde810ad0cdd4b8b051aed8c6ff250d9
ola.okejimi <ola.okejimi@ft.com>
2016-10-08 16:25:08 +0100

    Merge branch 'master' of /Users/ola.okejimi/IdeaProjects/asana-bot with conflicts.

--- src/main/java/com/ft/report/model/Criteria.java
+++ src/main/java/com/ft/report/model/Criteria.java
@@ -5,6 +5,7 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Data
 @AllArgsConstructor
@@ -13,4 +14,8 @@ public class Criteria {
     private ReportType reportType;
     private String team;
     private List<Project> projects;
+
+    public List<String> getProjectIds(){
+        return projects.stream().map(Project::getId).collect(Collectors.toList());
+    }
 }
